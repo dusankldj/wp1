2df4bff9243e1581cd787b315fe6bd38be26d6e6
dusankldj <127887854+dusankldj@users.noreply.github.com>
2023-12-10 16:06:22 +0100

    Update kontakt.js

--- assets/js/kontakt.js
+++ assets/js/kontakt.js
@@ -21,7 +21,7 @@ document.getElementById("navIkonice").innerHTML=navContentIkonice;
 
 
 
-
+//funkcija u koju prosledjujemo parametre regex izraza i polja koje proveravamo
 function proveriPolja(regEx,polje){
     if(!regEx.test(polje.value))
         polje.nextElementSibling.classList.remove('d-none');
@@ -29,11 +29,12 @@ function proveriPolja(regEx,polje){
         polje.nextElementSibling.classList.add('d-none');
 }
 
-
+//regularni izrazi
 let regImePrezime = /^[A-Z][a-z]{2,17}(\s[A-Z][a-z]{2,17})?$/;
 let regExEmail=/^[a-z0-9]{1,30}(\.|[a-z0-9])*@gmail\.com$/;
 let regTelefon = /^\+\d{1,14}$/;
 
+//pozivanje funkcije za svako polje koje proveravamo
 let ime=document.getElementById('name');
 document.getElementById('name').addEventListener("blur",function(){
     proveriPolja(regImePrezime,ime);
@@ -54,6 +55,7 @@ document.getElementById('phone').addEventListener("blur",function(){
     proveriPolja(regTelefon,brTel);
 });
 
+//provera checkBoxa
 var isChecked=false;
         var formErrors=0;
         var greskaPol=document.getElementById('greskaPol');
@@ -88,9 +90,10 @@ var isChecked=false;
 
 
 document.getElementById('readMore').addEventListener("click",function(){
-    submit();
+    submit();//dugme za proveru svih polja na kliktaj dugmeta submit
 });
 
+//provera za textarea
 function proveriMessage(){
     let textBox=document.getElementById('message').value;
     let poruka=document.getElementById('greskaPoruka');
@@ -112,6 +115,8 @@ function submit(){
         proveraCheckBoxa();
 }
 
+
+//prikazivanje/sakrivanje dugmeta Close za licence agreement
 let licence=document.getElementById('licence');
 let agreementBlock=document.getElementById('agreementBlock');
 licence.addEventListener('click',function(){
@@ -122,6 +127,18 @@ let closeLicenceButton=document.getElementById('closeLicence');
 closeLicenceButton.addEventListener('click',function(){
     agreementBlock.classList.toggle('d-none');
 });
+
+
+/*plug in*/
+$(document).ready(function() {
+    $(".accordion-content").css("display", "none");
+});
+$(".accordion-title").click(function () {
+          $(".accordion-title").not(this).removeClass("open");
+          $(".accordion-title").not(this).next().slideUp(300);
+          $(this).toggleClass("open");
+          $(this).next().slideToggle(300);
+        });
 
 
 
@@ -136,6 +153,26 @@ for(let i=0;i<3;i++){
     ikoniceFuter+=`<li><a href="${futerIkonice[i].link}" target="_blank"><i class="fa fa-${futerIkonice[i].imeIkonice}"></i></a></li>`;
 }
 document.getElementById("futerIkonice").innerHTML=ikoniceFuter;
+
+//dinamicko ispisivanje uslova koriscenja
+let objFAQ=[
+    {tekst:"Product Usage: The products purchased from our store are intended for personal use only and should not be resold or used for commercial purposes without our explicit consent."},
+    {tekst:"Warranty: We provide a limited warranty for the functionality of our products. Any physical damage or unauthorized modifications will void the warranty."},
+    {tekst:"Software and Updates: Any software provided with the products is subject to the respective software licenses. Updates may be available and we encourage regular updates for security and performance reasons."},
+    {tekst:"Privacy and Data: We respect your privacy and handle all personal data in accordance with our privacy policy. Any data collected during transactions will be used solely for the purpose of completing your purchase."},
+    {tekst:"Returns and Refunds: Our store has a return and refund policy. Please refer to our policies for detailed information on returns, exchanges, and refunds."},
+    {tekst:"Liability: Our liability is limited to the cost of the purchased product. We are not responsible for any indirect, incidental, or consequential damages arising from the use of our products."},
+    {tekst:"Customer Support: We strive to provide excellent customer support. For any queries or assistance, feel free to contact our support team."},
+    {tekst:"Governing Law: Any disputes arising from this agreement shall be governed by the laws of the jurisdiction in which our store is located."},
+    {tekst:"Modifications: We reserve the right to modify or update these terms and conditions at any time. Any changes will be effective immediately upon posting."},
+    {tekst:"Acceptance: By using our products, you acknowledge that you have read and agreed to these terms and conditions."}
+];
+let uslovi="";
+for(let i=0;i<objFAQ.length;i++)
+    uslovi+=`<li class="rounded">${objFAQ[i].tekst}</li>`;
+let usloviKoriscenja=document.querySelector("#listaLicence").innerHTML=uslovi;
+
+
 
 let navFuter=[
     {tekst:"Home", link:"index.html"},
