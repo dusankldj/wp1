2fc1035f3bcd2adfbf080cbef74c46ac23702d2a
dusankldj <127887854+dusankldj@users.noreply.github.com>
2023-12-12 16:05:03 +0100

    Update glavna.js

--- assets/js/glavna.js
+++ assets/js/glavna.js
@@ -16,24 +16,23 @@ for(let i=0;i<nizIkonicaNavPutanja.length;i++)
     navContentIkonice+=`<li><img src="${nizIkonicaNavPutanja[i]}" alt="${nizIkonicaNavAlt[i]}">${nizIkonicaNavText[i]}</li>`;
 document.getElementById("navIkonice").innerHTML=navContentIkonice;
 
-//Dinamicko ispisivanje blokova
 let blokTelefon="";
 let objTel=[
-    {slika:"1.png",model:"iPhone 13 Pro Max", cena:1000},
-    {slika:"2.png",model:"Galaxy S21 Ultra", cena:950},
-    {slika:"3.png",model:"OnePlus 9 Pro", cena:875},
-    {slika:"4.png",model:"Xiaomi Mi 11 Ultra", cena:850},
-    {slika:"5.png",model:"Sony Xperia 1 III", cena:900},
-    {slika:"6.png",model:"Motorola Edge+", cena:790}
+    {slika:"1.png",model:"iPhone 13 Pro Max", cena:1000, id:1},
+    {slika:"2.png",model:"Galaxy S21 Ultra", cena:950,id:2},
+    {slika:"3.png",model:"OnePlus 9 Pro", cena:875,id:3},
+    {slika:"4.png",model:"Xiaomi Mi 11 Ultra", cena:850,id:4},
+    {slika:"5.png",model:"Sony Xperia 1 III", cena:900,id:5},
+    {slika:"6.png",model:"Motorola Edge+", cena:790,id:6}
 ];
 for(let i=0;i<objTel.length;i++){
     blokTelefon+=`<div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 margin">
-                  <div class="brand_box rounded">
+                  <div class="brand_box rounded font">
                   <img src="assets/images/${objTel[i].slika}" alt="img" />
                   <ul class="no-bullets">
                   <li><h2 class="mt-4">${objTel[i].model}</h2></li>
                   <li> <h3>$<strong class="red">${objTel[i].cena}</strong></h3></li>
-                  <li><button type="button" class="readMore crveno-bg btn my-4"  data-bs-toggle="modal" data-bs-target="#iskacuciProzor">
+                  <li><button data-id="${objTel[i].id}" type="button" class="readMore btn crveno-bg my-4"  data-bs-toggle="modal" data-bs-target="#iskacuciProzor">
                   Read more
                   </button></li>
                   </ul>
@@ -42,17 +41,25 @@ for(let i=0;i<objTel.length;i++){
 }
 document.getElementById("telefoni").innerHTML=blokTelefon;
 
+
+
+
+
 let objNovihTel=[
-    {slika:"7.jpg",model:"Asus ROG Phone 5",cena:700},
-    {slika:"8.png",model:"Realme GT",cena:680},
-    {slika:"9.jpg",model:"Motorola Edge 20 Pro",cena:820},
-    {slika:"10.png",model:"Oppo Find X3 Pro",cena:700},
-    {slika:"11.jpg",model:"Vivo X60 Pro+",cena:725},
-    {slika:"12.jpg",model:"Lenovo Legion Phone Duel 2",cena:780}
+    {slika:"7.jpg",model:"Asus ROG Phone 5",cena:700,id:7},
+    {slika:"8.png",model:"Realme GT",cena:680,id:8},
+    {slika:"9.jpg",model:"Motorola Edge 20 Pro",cena:820,id:9},
+    {slika:"10.png",model:"Oppo Find X3 Pro",cena:700,id:10},
+    {slika:"11.jpg",model:"Vivo X60 Pro+",cena:725,id:11},
+    {slika:"12.jpg",model:"Lenovo Legion Phone Duel 2",cena:780,id:12}
 ];
 
+/*<h2 class="mt-4">${objTel[i].model}</h2>
+                  <h3>$<strong class="red">100</strong></h3>
+                  <button type="button" class="btn btn-danger my-2" data-bs-toggle="modal" data-bs-target="#iskacuciProzor">
+                  Klikni me
+                  </button>*/
 
-//funkcija koja na klik dugmeta see more prikazuje/sakriva novih 6 blokova
 let brojac=0;
 function prikaziSakrij(){
     brojac++;
@@ -75,13 +82,14 @@ function prikaziSakrij(){
             <ul class="no-bullets">
             <li><h2 class="mt-4">${objNovihTel[i].model}</h2></li>
             <li> <h3>$<strong class="red">${objNovihTel[i].cena}</strong></h3></li>
-            <li><button  type="button" class="readMore crveno-bg btn my-4" data-bs-toggle="modal" data-bs-target="#iskacuciProzor">
+            <li><button type="button" class="readMore btn crveno-bg my-4" data-bs-toggle="modal" data-id="${objNovihTel[i].id}" data-bs-target="#iskacuciProzor">
             Read more
             </button></li>
             </ul>
             </div>
             </div>`;
             $('#holderNoviBlokovi').append(noviBlok);
+            dodajEventove();
         }
     }
     function ukloniBlokove(){
@@ -90,7 +98,7 @@ function prikaziSakrij(){
 }     
 document.getElementById("vidiJos").addEventListener("click",prikaziSakrij);    
  
-//objekat sa sadrzajem modala
+//objekat modala sa informacijama
 var objModala=[
     {slika:"1.png",model:"iPhone 13 Pro Max", cena:"1000$", ram:"16GB",cpu:"snapdragon",baterry:"5000mAh"},
     {slika:"2.png",model:"Galaxy S21 Ultra", cena:"950$", ram:"12GB",cpu:"snapdragon",baterry:"5000mAh"},
